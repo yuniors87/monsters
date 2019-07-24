e6ae355f214b31deef4456d1295dda6413b8a81b
yuniors87 <yuniors87@gmail.com>
2019-07-24 05:42:33 -0400

    Updates

---
+++ precache-manifest.434c93c8c2e6756268398fa46728e6d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848d584fca9ff052e0344b21ffa53d42",
+    "url": "/github.io/monsters/index.html"
+  },
+  {
+    "revision": "c65b31c53629e1f0c7fd",
+    "url": "/github.io/monsters/static/css/main.bda07ada.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/github.io/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c65b31c53629e1f0c7fd",
+    "url": "/github.io/monsters/static/js/main.db674e38.chunk.js"
+  },
+  {
+    "revision": "24f48834a961bfd9441d",
+    "url": "/github.io/monsters/static/js/runtime~main.66cf782e.js"
+  }
+]);
